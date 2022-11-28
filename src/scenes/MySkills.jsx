01214f5b7d68b8498c7be04179aeee29b2becbf9
JLCalda <jlcaldadev23@gmail.com>
2022-11-28 23:00:29 +0800

    Added skills section

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -47,6 +47,93 @@ const MySkills = ({ isAboveMediumScreens }) => {
           )}
         </div>
       </div>
+      {/* SKILLS */}
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/* EXPERIENCE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playFair font-semibold text-5xl">01</p>
+              <p className="font-playFair font-semibold text-3xl mt-3">
+                Experience
+              </p>
+            </div>
+          </div>
+          <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]"></div>
+          <p className="mt-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem odit
+            sapiente voluptas fugiat saepe ipsa laboriosam quas eius magni,
+            distinctio dicta? Nihil magni veniam temporibus error maxime sunt
+            laborum ipsa.
+          </p>
+        </motion.div>
+        {/* INNOVATIVE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playFair font-semibold text-5xl">02</p>
+              <p className="font-playFair font-semibold text-3xl mt-3">
+                Innovative
+              </p>
+            </div>
+          </div>
+          <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]"></div>
+          <p className="mt-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem odit
+            sapiente voluptas fugiat saepe ipsa laboriosam quas eius magni,
+            distinctio dicta? Nihil magni veniam temporibus error maxime sunt
+            laborum ipsa.
+          </p>
+        </motion.div>
+        {/* IMAGINATIVE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playFair font-semibold text-5xl">03</p>
+              <p className="font-playFair font-semibold text-3xl mt-3">
+                Imaginative
+              </p>
+            </div>
+          </div>
+          <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]"></div>
+          <p className="mt-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem odit
+            sapiente voluptas fugiat saepe ipsa laboriosam quas eius magni,
+            distinctio dicta? Nihil magni veniam temporibus error maxime sunt
+            laborum ipsa.
+          </p>
+        </motion.div>
+      </div>
     </section>
   );
 };
